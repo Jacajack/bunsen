132985221c3488afdb66c3d10bd9eca9ecbac6f3
Jacek Wieczorek <mrjjot@gmail.com>
2021-02-16 03:29:34 +0100

    Add some messy editor UI stuff

--- src/editor/editor.cpp
+++ src/editor/editor.cpp
@@ -5,6 +5,7 @@
 #include "../assimp_loader.hpp"
 #include "ui/ui.hpp"
 #include "../log.hpp"
+#include <imgui_internal.h>
 using bu::bunsen_editor;
 
 /**
@@ -144,13 +145,25 @@ static void ui_scene_graph(const bu::scene &scene, std::list<std::weak_ptr<bu::s
 
 		if (display_node)
 		{
-			node_open = ImGui::TreeNodeEx(node_ptr.get(), node_flags, "%s", node_ptr->get_name().c_str());
+			std::string name_tags;
+			if (dynamic_cast<bu::mesh_node*>(node_ptr.get()))
+				name_tags += "[M] ";
+
+			if (dynamic_cast<bu::light_node*>(node_ptr.get()))
+				name_tags += "[L] ";
+
+			if (dynamic_cast<bu::camera_node*>(node_ptr.get()))
+				name_tags += "[C] ";
+
+			node_open = ImGui::TreeNodeEx(node_ptr.get(), node_flags, "%s%s", name_tags.c_str(), node_ptr->get_name().c_str());
 			
-			// On normal click - select or deselect
+			// On click - select or deselect
 			// Ctrl + click - toggle
+			// If multiple items are selected, and control is not held - select
 			if (ImGui::IsItemClicked())
 			{
 				bool already_selected = selection_contains(node_ptr);
+				bool multiple_selection = selection.size() > 1;
 				if (ImGui::GetIO().KeyCtrl)
 				{
 					if (already_selected) selection_remove(node_ptr);
@@ -159,7 +172,7 @@ static void ui_scene_graph(const bu::scene &scene, std::list<std::weak_ptr<bu::s
 				else
 				{
 					selection.clear();
-					if (!already_selected) selection.push_back(node_ptr);
+					if (multiple_selection || !already_selected) selection.push_back(node_ptr);
 				}
 			}
 		}
@@ -185,13 +198,209 @@ static void ui_scene_graph(const bu::scene &scene, std::list<std::weak_ptr<bu::s
 
 				for (const auto &mesh : mesh_node_ptr->meshes)
 				{
-					ImGui::TreeNodeEx(&mesh, leaf_flags, "[M] %s", mesh.data->name.c_str());
+					ImGui::TreeNodeEx(&mesh, leaf_flags, "[mesh] %s", mesh.data->name.c_str());
 				}
 			}
 		}
 	}
 
 	ImGui::EndChild();
+}
+
+/**
+	\brief Property editor for nodes (transforms, names)
+*/
+static void ui_node_properties(std::shared_ptr<bu::scene_node> node)
+{
+	bool visible = node->is_visible();
+	bool local_transform = node->get_transform_origin() == bu::scene_node::node_transform_origin::PARENT;
+	static glm::vec3 translate;
+	static glm::vec3 rotation;
+	// static float scale;
+	
+
+	std::string name = node->get_name();
+	std::vector<char> buf(std::max(32u, static_cast<unsigned int>(name.size()) + 1));
+	std::copy(name.begin(), name.end(), buf.begin());
+
+
+	glm::mat4 mat = node->get_transform();
+	translate = mat[3];
+	mat[3] = glm::vec4{0.f};
+	glm::vec3 scale{glm::length(mat[0]), glm::length(mat[1]), glm::length(mat[2])};
+	mat[0] /= scale.x;
+	mat[1] /= scale.y;
+	mat[2] /= scale.z;
+	auto quat = glm::quat_cast(mat);
+
+	// if (ImGui::CollapsingHeader("Node properties"))
+	{
+		// ImGui::Separator();
+		// ImGui::Dummy(ImVec2(0.f, 5.f));
+		// ImGui::Text("Node property editor");
+		ImGui::InputText("Name", buf.data(), 32);
+		ImGui::Dummy(ImVec2(0.f, 10.f));
+		
+		ImGui::DragFloat3("Translation", &translate[0], 0.1f, 0, 0);
+		ImGui::DragFloat4("Rotation", &quat[0], 0.1f, 0, 0);
+		ImGui::DragFloat3("Scale", &scale[0], 0.1f, 0, 0);
+		ImGui::Dummy(ImVec2(0.f, 10.f));
+
+		// ImGui::SameLine(ImGui::GetWindowContentRegionWidth() - 30);
+		ImGui::Checkbox("Local transform", &local_transform);
+		ImGui::Checkbox("Visible", &visible);
+	}
+
+	quat = glm::normalize(quat);
+	mat = glm::translate(glm::scale(glm::mat4_cast(quat), scale), translate);
+
+	node->set_visible(visible);
+	node->set_transform_origin(local_transform ? bu::scene_node::node_transform_origin::PARENT : bu::scene_node::node_transform_origin::WORLD);
+	// node->set_transform(mat);
+	node->set_name(std::string{buf.begin(), buf.end()});
+}
+
+/**
+	\brief Node controls (grouping and deleting)
+*/
+static void ui_node_controls(const bu::scene &scene, std::list<std::weak_ptr<bu::scene_node>> &selection)
+{
+	bool selection_empty = selection.empty();
+	bool multiple_selection = selection.size() > 1;
+	bool contains_root_node = false;
+	bool have_common_parent = true;
+	std::shared_ptr<bu::scene_node> common_parent;
+	std::set<std::shared_ptr<bu::scene_node>> nodes;
+
+	for (auto &wp : selection)
+	{
+		if (auto node = wp.lock())
+		{
+			nodes.insert(node);
+			if (node == scene.root_node) contains_root_node = true;
+			if (!common_parent)
+				common_parent = node->get_parent().lock();
+			else if (common_parent != node->get_parent().lock())
+				have_common_parent = false;
+		}
+	}
+
+	bool disable_delete = selection_empty || contains_root_node;
+	bool disable_dissolve = selection_empty || contains_root_node;
+	bool disable_group = selection_empty || contains_root_node || !have_common_parent || !common_parent;
+	bool disable_properties = multiple_selection || selection_empty;
+	bool disable_duplicate = selection_empty || contains_root_node;
+
+	auto push_disable = [](bool disable)
+	{
+		if (!disable) return;
+		ImGui::PushItemFlag(ImGuiItemFlags_Disabled, true);
+		ImGui::PushStyleVar(ImGuiStyleVar_Alpha, ImGui::GetStyle().Alpha * 0.5f);
+	};
+
+	auto pop_disable = [](bool disable)
+	{
+		if (!disable) return;
+		ImGui::PopItemFlag();
+		ImGui::PopStyleVar();
+	};
+
+	ImGui::Dummy(ImVec2(0.f, 10.f));
+
+	// Delete node
+	push_disable(disable_delete);
+	if (ImGui::Button("Delete node"))
+	{
+		for (auto &n : nodes)
+			n->remove_from_parent();
+		selection.clear();
+	}
+	pop_disable(disable_delete);
+
+	// Dissolve node
+	push_disable(disable_dissolve);
+	ImGui::SameLine();
+	if (ImGui::Button("Dissolve node"))
+	{
+		for (auto &n : nodes)
+			n->dissolve();
+		selection.clear();
+	}
+	pop_disable(disable_dissolve);
+
+	// Group nodes
+	push_disable(disable_group);
+	ImGui::SameLine();
+	if (ImGui::Button("Group nodes"))
+	{
+		auto group_node = std::make_shared<bu::scene_node>();
+		group_node->set_name("Group Node");
+		for (auto &n : nodes)
+			n->set_parent(group_node);
+		group_node->set_parent(common_parent);
+	}
+	pop_disable(disable_group);
+
+	// Duplicate nodes
+	// push_disable(disable_duplicate);
+	// ImGui::SameLine();
+	// if (ImGui::Button("Duplicate nodes"))
+	// {
+	// 	for (auto &n : nodes)
+	// 	{
+	// 		auto p = n->get_parent().lock();
+	// 		auto dup = std::make_shared<bu::scene_node>(*n);
+	// 		dup->set_name(dup->get_name() + " (dup)");
+	// 	}
+	// }
+	// pop_disable(disable_duplicate);
+
+
+	ImGui::Dummy(ImVec2(0.f, 10.f));
+
+	// Node properties (only if a single one is selected)
+	if (!disable_properties)
+	{
+		auto node = *nodes.begin();
+		if (ImGui::CollapsingHeader("Node properties"))
+			ui_node_properties(node);
+	}
+}
+
+/**
+	\brief Displays mesh info (vertex count, etc.) and list of all meshes in the selected nodes
+*/
+static void ui_mesh_info(const bu::scene &scene, std::list<std::weak_ptr<bu::scene_node>> &selection)
+{
+	std::set<std::shared_ptr<bu::scene_node>> nodes;
+	std::set<const bu::mesh*> meshes;
+
+	auto extract_meshes = [&meshes](const bu::scene_node &n)
+	{
+		auto extract_meshes_impl = [&meshes](const bu::scene_node &n, auto &ref)->void
+		{
+			if (auto mn = dynamic_cast<const bu::mesh_node*>(&n))
+				for (auto &mesh : mn->meshes)
+					meshes.insert(&mesh);
+			
+			for (const auto &c : n.get_children())
+				ref(*c, ref);
+		};
+
+		return extract_meshes_impl(n, extract_meshes_impl);
+	};
+
+	for (auto &wp : selection)
+		if (auto node = wp.lock())
+		{
+			nodes.insert(node);
+			extract_meshes(*node);
+		}
+
+	ImGui::Indent();
+	for (auto &mesh_ptr : meshes)
+		ImGui::BulletText("%s", mesh_ptr->data->name.c_str());
+	ImGui::Unindent();
 }
 
 /**
@@ -241,7 +450,12 @@ static void window_editor(bunsen_editor &ed)
 		}
 		
 		if (ed.scene)
+		{
 			ui_scene_graph(*ed.scene, ed.selected_nodes);
+			ui_node_controls(*ed.scene, ed.selected_nodes);		
+			if (ImGui::CollapsingHeader("Meshes info"))
+				ui_mesh_info(*ed.scene, ed.selected_nodes);
+		}
 	
 	}
 
