9f1de000a08e264ce7e20cd469ee99eee5aa90d4
Jacek Wieczorek <mrjjot@gmail.com>
2021-02-17 18:56:06 +0100

    Properly display non-uniformly scaled normals

--- resources/shaders/preview.vs.glsl
+++ resources/shaders/preview.vs.glsl
@@ -15,11 +15,22 @@ out struct VS_OUT
 
 void main()
 {
-	vec4 v_pos_view = mat_view * mat_model * vec4(v_pos, 1.0);
-	vec4 v_normal_view = mat_view * mat_model * vec4(v_normal, 0.0);
+	mat4 mat_mv = mat_view * mat_model;
+	vec4 v_pos_view = mat_mv * vec4(v_pos, 1.0);
+
+	/*
+		Compute overall scaling factor of the transform on each axis.
+		Dividing by scale squared is equivalent to computing normalized
+		rotation matrix first and then applying 1/s scaling to it.
+	*/
+	mat3 mat_mvn = mat3(mat_mv);
+	mat_mvn[0] /= dot(mat_mvn[0], mat_mvn[0]);
+	mat_mvn[1] /= dot(mat_mvn[1], mat_mvn[1]);
+	mat_mvn[2] /= dot(mat_mvn[2], mat_mvn[2]);
+	vec3 v_normal_view = normalize(mat_mvn * v_normal);
 
 	vs_out.v_pos = v_pos_view.xyz;
-	vs_out.v_normal = v_normal_view.xyz;
+	vs_out.v_normal = v_normal_view;
 	
 	gl_Position = mat_proj * v_pos_view;
 }
