bbc029b7cece67ed3e92b13329b363b4f92dc7dd
Jacek Wieczorek <mrjjot@gmail.com>
2021-02-19 21:25:31 +0100

    Fix outline shader covering entire screen with orange sometimes

--- resources/shaders/outline.gs.glsl
+++ resources/shaders/outline.gs.glsl
@@ -16,23 +16,19 @@ void main()
 {
 	const vec2 offset = vec2(0.003) * vec2(1, aspect);
 
+	if (gl_in[0].gl_Position.z > 0 || gl_in[1].gl_Position.z > 0 || gl_in[2].gl_Position.z > 0)
+		return;
+	
 	vec2 v0 = gl_in[0].gl_Position.xy;
 	vec2 v1 = gl_in[1].gl_Position.xy;
 	vec2 v2 = gl_in[2].gl_Position.xy;
 
 	vec2 E0 = v1 - v0;
 	vec2 E1 = v2 - v1;
-
-	// Reorder vertices if the triangle is back-facing
-	// We don't want the outline to be culled on thin backfacing objects
+	
+	// Discard back-facing triangles
 	if (E0.x * E1.y - E0.y * E1.x < 0)
-	{
-		vec2 v = v0;
-		v0 = v1;
-		v1 = v;
-		E0 = v1 - v0;
-		E1 = v2 - v1;
-	}
+		return;
 
 	vec2 E2 = v0 - v2;
 
