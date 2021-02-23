db63077ea27d3d5293f5f3605c577cf440c2be99
Jacek Wieczorek <mrjjot@gmail.com>
2021-02-23 22:07:42 +0100

    Make logging thread-safe

--- src/log.cpp
+++ src/log.cpp
@@ -2,3 +2,5 @@
 
 // Initialize reference time
 std::chrono::time_point<std::chrono::steady_clock> bu::log_stream::start_time = std::chrono::steady_clock::now();
+
+std::mutex bu::log_cerr_mutex;
--- src/log.hpp
+++ src/log.hpp
@@ -1,8 +1,9 @@
 #pragma once
-
 #include <iostream>
 #include <iomanip>
 #include <chrono>
+#include <mutex>
+#include <optional>
 #include "utils.hpp"
 
 namespace bu {
@@ -19,10 +20,13 @@ struct dummy_stream
 class log_stream : private std::streambuf, public std::ostream
 {
 public:
-	log_stream(std::ostream &output, const std::string &prefix, int color) : 
+	log_stream(std::ostream &output, const std::string &prefix, int color, std::mutex *mutex = nullptr) : 
 		std::ostream(this),
 		m_output(output)
 	{
+		if (mutex)
+			m_lock.emplace(*mutex);
+
 		auto duration = std::chrono::duration<double>(std::chrono::steady_clock::now() - start_time);
 		double s = duration.count();
 		m_output << "\x1b[92m[" << std::fixed << std::setprecision(5) << s << "]\x1b[0m ";
@@ -32,6 +36,7 @@ public:
 	~log_stream()
 	{
 		m_output << "\x1b[0m" << std::endl;
+		m_lock.reset();
 	}
 
 	static std::chrono::time_point<std::chrono::steady_clock> start_time;
@@ -49,33 +54,36 @@ private:
 	}
 
 	std::ostream &m_output;
+	std::optional<std::lock_guard<std::mutex>> m_lock;
 };
+
+extern std::mutex log_cerr_mutex;
 
 struct log_error : public log_stream
 {
 	log_error() :
-		log_stream(std::cerr, "error: ", 1)
+		log_stream(std::cerr, "error: ", 1, &log_cerr_mutex)
 	{}
 };
 
 struct log_warning : public log_stream
 {
 	log_warning() :
-		log_stream(std::cerr, "warning: ", 3)
+		log_stream(std::cerr, "warning: ", 3, &log_cerr_mutex)
 	{}
 };
 
 struct log_info : public log_stream
 {
 	log_info() :
-		log_stream(std::cerr, "info: ", 4)
+		log_stream(std::cerr, "info: ", 4, &log_cerr_mutex)
 	{}
 };
 
 struct log_debug : public log_stream
 {
 	log_debug() :
-		log_stream(std::cerr, "debug: ", 5)
+		log_stream(std::cerr, "debug: ", 5, &log_cerr_mutex)
 	{}
 };
 
@@ -95,7 +103,7 @@ struct log_debug : public log_stream
 
 // Debug logging is fully optional
 #ifdef DEBUG_LOGGING
-#define LOG_DEBUG bu::log_stream{std::cerr, "debug: " __FILE__ ":" BU_TO_STR(__LINE__) ": ", 5}
+#define LOG_DEBUG bu::log_stream{std::cerr, "debug: " __FILE__ ":" BU_TO_STR(__LINE__) ": ", 5, &bu::log_cerr_mutex}
 #else
 #define LOG_DEBUG bu::dummy_stream{}
 #endif
