74e263f2c7a40a0d094100b9c7dcf546667691aa
Jacek Wieczorek <mrjjot@gmail.com>
2021-11-14 18:56:41 +0100

    Slight BVH optimization [untested]

--- src/renderers/rt/bvh.cpp
+++ src/renderers/rt/bvh.cpp
@@ -20,13 +20,13 @@ bvh_tree::~bvh_tree()
 
 bool bvh_tree::test_ray(const rt::ray &r, rt::ray_hit &hit) const
 {
-	bu::rt::linear_stack<unsigned int, 32> st;
+	bu::rt::linear_stack<std::pair<unsigned int, float>, 32> st;
 	
 	// Check intersection with the root node
 	{
 		float t;
 		if (nodes[1].aabb.test_ray(r, t))
-			st.push(1);
+			st.push({1, t});
 		else
 			return false;
 	}
@@ -38,9 +38,11 @@ bool bvh_tree::test_ray(const rt::ray &r, rt::ray_hit &hit) const
 	while (!st.empty())
 	{
 		// Get node
-		auto node_id = st.top();
+		auto [node_id, node_t] = st.top();
 		auto &node = nodes[node_id];
 		st.pop();
+
+		if (node_t >= best.t) continue;
 
 		// Negative count indicates no children an no triangles
 		// Zero count indicates node with children
@@ -55,25 +57,31 @@ bool bvh_tree::test_ray(const rt::ray &r, rt::ray_hit &hit) const
 			float tl, tr;
 			bool hit_l = nl.aabb.test_ray(r, tl);
 			bool hit_r = nr.aabb.test_ray(r, tr);
+			hit_l = hit_l && tl < best.t;
+			hit_r = hit_r && tr < best.t;
 
 			if (hit_l && hit_r) // Both children were hit
 			{
 				// Check the closer child first
 				if (tl < tr)
 				{
-					if (tr < best.t) st.push(id_r);
-					if (tl < best.t) st.push(id_l);
+					st.push({id_r, tr});
+					st.push({id_l, tl});
 				}
 				else
 				{
-					if (tl < best.t) st.push(id_l);
-					if (tr < best.t) st.push(id_r);
+					st.push({id_l, tl});
+					st.push({id_r, tr});
 				}
 			}
 			else if (hit_l)
-				st.push(id_l);
+			{
+				st.push({id_l, tl});
+			}
 			else if (hit_r)
-				st.push(id_r);
+			{
+				st.push({id_r, tr});
+			}
 		}
 		else if (node.count > 0) // Leaf node
 		{
